f5209d7da011d4ff24eacceb5f4b5fa7aed46627
KJ Loving <kjloving1400@gmail.com>
2024-09-03 17:19:08 -0700

    js fixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "@rails/ujs"; // Import Rails UJS
+Rails.start(); 
+
 import "@hotwired/turbo-rails";
 import "@hotwired/stimulus";
 import "@hotwired/stimulus-loading";
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,31 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+20.times do
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::Number.number(digits: 10)
+  )
+end
+
+puts "Created 20 fake customers"
+
+customers = Customer.all
+
+if customers.any?
+  20.times do
+    Order.create!(
+      product_name: Faker::Commerce.product_name,    # Generates a random product name
+      product_count: Faker::Number.between(from: 1, to: 100), # Generates a random product count between 1 and 100
+      customer_id: customers.sample.id               # Randomly selects a customer ID from existing customers
+    )
+  end
+
+  puts "Created 20 fake orders"
+else
+  puts "No customers found. Please seed customers first."
+end
